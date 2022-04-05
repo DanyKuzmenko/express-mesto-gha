cacd16b3c80e672449754b5408e8414d419f8b14
DanyKuzmenko <sh0k555@yandex.ru>
2022-04-05 20:10:29 +0300

    fix: errorHandler

--- errors/errorHandler.js
+++ errors/errorHandler.js
@@ -1,17 +1,13 @@
-// eslint-disable-next-line no-unused-vars,consistent-return
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   const status = err.statusCode || 500;
-  // if (err.name === 'CastError') {
-  //   return res.status(401)
-  //     .send({ message: 'Передан некорректный id' });
-  // }
+
   if (err.name === 'ValidationError' || err.name === 'CastError') {
     return res.status(400)
       .send({ message: 'Переданы некорректные данные' });
   }
-  res.status(status)
+  return res.status(status)
     .send({ message: err.message });
-  next();
 };
 
 module.exports = errorHandler;
