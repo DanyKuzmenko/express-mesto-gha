edee7ac4d8a14f485560cb4b5235022527f78216
Dany Kuzmenko <sh0k555@yandex.ru>
2022-04-13 00:47:01 +0300

    feature: add CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const ErrorNotFound = require('./errors/ErrorNotFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { cors } = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
@@ -16,6 +17,8 @@ const app = express();
 app.use(express.json());
 
 app.use(requestLogger);
+
+app.use(cors());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
